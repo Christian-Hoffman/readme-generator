7ecb6fc07354a0a3ab3e3de832655dc578b8fa35
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-09-08 11:57:37 -0400

    added prompt questions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const prompt = inquirer.createPromptModule();
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -11,33 +16,75 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-  ## Table on Contents
-  [Description](description)
-  [Installation Instructions](installation instructions)
-  [Usage Information](usage information)
-  [Contribution Guidelines](contribution guidelines)
-  [Test Instructions](test instructions)
-  [License](license)
-  [Questions](questions)
+inquirer.prompt([
+    {
+        name: 'title',
+        message: 'What would you like your title to be?'
+    },
+    {
+        name: 'license',
+        message: 'What license will you use?',
+        type: 'rawlist',
+        choice: [
+            'MIT',
+            '',
+            ''
+        ],
+    },
+    {
+        name: 'Description',
+        message: 'Write your description here'
+    },
+    {
+        name: 'Installation',
+        message: 'Write your installation instructions here'
+    },
+    {
+        name: 'Usage',
+        message: 'Write your usage information here'
+    },
+    {
+        name: 'Contribution',
+        message: 'Write your contribution guidelines'
+    },
+    {
+        name: 'Test',
+        message: 'Write your test instructions here'
+    },
+    {
+        name: 'Questions',
+        message: 'Write your questions here'
+    },
+])
+.then(function generateMarkdown(data) {
+      return `# ${data.title}
+    
+      ## Table on Contents
+      [Description](description)
+      [Installation Instructions](installation instructions)
+      [Usage Information](usage information)
+      [Contribution Guidelines](contribution guidelines)
+      [Test Instructions](test instructions)
+      [License](license)
+      [Questions](questions)
+    
+      ## Description
+        ${data.description}
+      ## Installation
+        ${data.installation}
+      ## Usage Information
+        ${data.usage}
+      ## Contribution Guidelines
+        ${data.contribution}
+      ## Test Instructions
+        ${data.test}
+      ## License
+        ${data.license}
+      ## Questions
+        ${data.questions}
+    `;
+    
+    }).then((data) => fs.writeFileSync('README.md', data, 'utf8'));
 
-  ## Description
-
-  ## Installation
-
-  ## Usage Information
-
-  ## Contribution Guidelines
-
-  ## Test Instructions
-
-  ## License
-
-  ## Questions
-`;
-}
-
-module.exports = generateMarkdown;
-
+    module.exports = generateMarkdown;
